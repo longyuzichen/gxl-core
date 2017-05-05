c759d5a12f4e04526811bb949810e3731b2d9736
longyuzichen <523974882@qq.com>
2017-05-06 00:35:53 +0800

    修改实体类，添加默认返回状态码

--- src/main/java/com/longyuzichen/core/common/Result.java
+++ src/main/java/com/longyuzichen/core/common/Result.java
@@ -11,40 +11,37 @@ import java.util.Map;
  * @desc 结果集
  * @date 2017-03-25 19:56
  */
-public class Result implements Serializable{
-    private int code ; //返回状态码
+public class Result implements Serializable {
+    private int code = 200;//状态码
     private String message; //返回信息
 
     private String result; //业务结果
     private String error; //错误代码
-    private String desc ; //错误代码信息
+    private String desc; //错误代码信息
 
-    private Map<String,Object> data; //信息集
+    private Map<String, Object> data; //信息集
 
-    public Result(int code) {
+   /* public Result(int code) {
         this.code = code;
-    }
+    }*/
 
     public Result(int code, String message) {
         this.code = code;
         this.message = message;
     }
 
-    public Result(int code, String result, String error) {
-        this.code = code;
+    public Result(String result, String error) {
         this.result = result;
         this.error = error;
     }
 
-    public Result(int code, String result, String error, String desc) {
-        this.code = code;
+    public Result( String result, String error, String desc) {
         this.result = result;
         this.error = error;
         this.desc = desc;
     }
 
-    public Result(int code, String result, Map<String, Object> data) {
-        this.code = code;
+    public Result( String result, Map<String, Object> data) {
         this.result = result;
         this.data = data;
     }
