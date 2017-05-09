23c9f0f7306eebb908e3800b5de37670cdf6ca31
longyuzichen <523974882@qq.com>
2017-05-09 20:40:19 +0800

    测试类

---
+++ src/test/java/com/longyuzichen/core/code/RSATest.java
@@ -0,0 +1,42 @@
+package com.longyuzichen.core.code;
+
+import org.junit.Test;
+
+import java.io.*;
+import java.security.interfaces.RSAPrivateKey;
+
+import static org.junit.Assert.*;
+
+/**
+ * com.longyuzichen.core.code
+ *
+ * @author longyuzichen@126.com
+ * @version V1.0
+ * @desc
+ * @date 2017-05-06 21:23
+ */
+public class RSATest {
+    @Test
+    public void rsaSign() throws Exception {
+    }
+
+    @Test
+    public void verifyRSASign() throws Exception {
+    }
+
+    @Test
+    public void getPrivateKey() throws Exception {
+         /*  RSAPrivateKey key = RSA.getPrivateKey();
+        File file = new File("D://key.txt");
+        FileOutputStream os = new FileOutputStream(new String(key.getEncoded()));
+        byte[] by = new byte[1024];
+        int i = 0;
+       // BufferedInputStream is = new BufferedInputStream();
+        System.out.print(key.toString());*/
+    }
+
+    @Test
+    public void getPublicKey() throws Exception {
+    }
+
+}
---
+++ src/test/java/com/longyuzichen/core/common/IdentifyingCodeTest.java
@@ -0,0 +1,60 @@
+package com.longyuzichen.core.common;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * com.longyuzichen.core.common
+ *
+ * @author longyuzichen@126.com
+ * @version V1.0
+ * @desc
+ * @date 2017-04-08 23:08
+ */
+public class IdentifyingCodeTest {
+    @Test
+    public void verifyCodeStream() throws Exception {
+    }
+
+    @Test
+    public void verifyCodeStream1() throws Exception {
+    }
+
+    @Test
+    public void verifyCodeStream2() throws Exception {
+    }
+
+    @Test
+    public void verifyCodeStream3() throws Exception {
+    }
+
+    @Test
+    public void verifyCodeStream4() throws Exception {
+    }
+
+    @Test
+    public void verifyCodeFile() throws Exception {
+    }
+
+    @Test
+    public void createCode() throws Exception {
+    }
+
+    @Test
+    public void createCode1() throws Exception {
+    }
+
+    @Test
+    public void createCode2() throws Exception {
+    }
+
+    @Test
+    public void getRandomNumber() throws Exception {
+    }
+
+    @Test
+    public void getRandomColor() throws Exception {
+    }
+
+}
