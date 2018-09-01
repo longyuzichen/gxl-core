c309b863cc77fc2fb9ea93743dc93fb71aaef75a
longyuzichen <523974882@qq.com>
2018-09-02 00:14:04 +0800

    删除多余导入类名称

--- src/main/java/com/longyuzichen/core/util/StringUtil.java
+++ src/main/java/com/longyuzichen/core/util/StringUtil.java
@@ -19,10 +19,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * @desc 字符串工具
